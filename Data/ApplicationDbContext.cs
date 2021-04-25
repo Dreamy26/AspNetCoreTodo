df6b86659b4cd8f531943a5669e8654d8aea8320
AWilson <dreamy26@icloud.com>
2021-04-24 23:48:10 -0400

    worked on database and added an additional class

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -24,5 +24,4 @@ namespace AspNetCoreTodo.Data
         
     }
 
-    
 }
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace AspNetCoreTodo.Models
+{
+    public class ApplicationUser
+    {
+    }
+}
